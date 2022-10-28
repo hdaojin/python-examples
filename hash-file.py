122942c2b993503b06b86f9a26eaceddce4f2dcb
hdaojin <hdaojin@hotmail.com>
2022-10-28 13:01:36 +0800

    modify: hash-file.py add progressbar function

--- hash-file.py
+++ hash-file.py
@@ -1,6 +1,9 @@
 import io
 import sys
 import hashlib
+from pathlib import Path
+
+from tqdm import tqdm
 
 def hash_sum(path):
     def read_chunks(file, size=io.DEFAULT_BUFFER_SIZE):
@@ -19,13 +22,26 @@ def hash_sum(path):
         h = hashlib.sha256()
         length = 0
         with open(path, 'rb') as f:
-            for block in read_chunks(f, size=blocksize):
-                length += len(block)
-                h.update(block)
+            with tqdm(total=Path(path).stat().st_size, unit='B', unit_divisor=1024, desc="Hashing", unit_scale=True) as pbar:
+                for block in read_chunks(f, size=blocksize):
+                    length += len(block)
+                    h.update(block)
+                    pbar.update(len(block))
         return h, length
     return hash_file(path)
 
-path = sys.argv[1]
-hash_value, file_size = hash_sum(path)
-print("Sha256: ", hash_value.hexdigest())
-print("Size:   ", file_size)
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+    else:
+        print("Usage: python3 hash-file.py FILE")
+        sys.exit(1)
+
+    try :
+        hash_value, file_size = hash_sum(path)
+    except OSError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+    else:
+        print("Sha256: ", hash_value.hexdigest())
+        print("Size: ", file_size)
